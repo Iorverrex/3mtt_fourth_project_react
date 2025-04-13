30b110c4c3b49b1a389948ad790fc12fcfba647f
Iorverrex <iorvertersur@gmail.com>
2025-04-13 22:46:28 +0100

    my fifth commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-
+import Counter from "./Counter";
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <h2>Hello world!</h2>
+      <h1>My Counter App</h1>
+      <Counter />
     </div>
   );
 }
